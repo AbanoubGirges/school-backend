b5dd82983e32e83b3a5f61ce19f406fef4ab4ac6
bebofcds <bebo.boby3@gmail.com>
2026-05-16 02:11:49 +0300

    added a message for duplicate attendance

--- Controllers/adminControllers/attendanceControllers/createAttendanceController.ts
+++ Controllers/adminControllers/attendanceControllers/createAttendanceController.ts
@@ -2,7 +2,11 @@ import express from "express";
 import { createAttendanceRecord } from "../../../repo/attendanceQueries.ts";
 import { validationResult } from "express-validator";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
-const createAttendanceController = async (req: express.Request, res: express.Response) => {
+import { Prisma } from "@prisma/client";
+const createAttendanceController = async (
+  req: express.Request,
+  res: express.Response,
+) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400).json({ errors: errors.array() });
@@ -20,18 +24,30 @@ const createAttendanceController = async (req: express.Request, res: express.Res
       id,
       today,
       status.toUpperCase(),
-      note
+      note,
     );
     res
       .status(201)
       .json({ message: "ATTENDANCE_RECORD_CREATED", data: attendanceRecord });
   } catch (err) {
-    if (err instanceof PrismaClientKnownRequestError && err.message === "USER_NOT_FOUND") {
+    if (
+      err instanceof PrismaClientKnownRequestError &&
+      err.message === "USER_NOT_FOUND"
+    ) {
       res.status(404).json({ message: "USER_NOT_FOUND" });
       return;
     }
+    if (
+      err instanceof Prisma.PrismaClientKnownRequestError &&
+      err.code === "P2002"
+    ) {
+      res.status(409).json({
+        error: `ATTENDANCE_ALREADY_TAKEN_FOR_TODAY`,
+      });
+      return;
+    }
     console.error("Error creating attendance record:", err);
-    res.status(500).json({ message: "ERROR_CREATING_ATTENDANCE"});
+    res.status(500).json({ message: "ERROR_CREATING_ATTENDANCE" });
   }
 };
 export { createAttendanceController };
