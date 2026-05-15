de5d210ce1b7b7cb09e42210e3aa6590aa36b7ec
bebofcds <bebo.boby3@gmail.com>
2026-05-16 00:44:01 +0300

    fixed empty request on update status

--- routes/adminUserRouter.ts
+++ routes/adminUserRouter.ts
@@ -3,9 +3,10 @@ import authAdmin from "../middleware/authAdmin.ts";
 import { updateStatusController } from "../Controllers/adminControllers/onUser/updateStatusController.ts";
 import { getPendingController } from "../Controllers/adminControllers/onUser/getPendingController.ts";
 import { fetchUserController } from "../Controllers/adminControllers/onUser/fetchUserController.ts";
+import { body } from "express-validator";
 const adminUserRouter = express.Router();
 adminUserRouter.use(authAdmin);
-adminUserRouter.patch("/:id/status", updateStatusController);
+adminUserRouter.patch("/:id/status",[body("status").isIn(["APPROVED", "REJECTED", "PENDING"])], updateStatusController);
 adminUserRouter.get("/pending", getPendingController);
 adminUserRouter.get("/:id", fetchUserController);
 
