91468972544acd08bb27971529aeb2c86266a89c
bebofcds <bebo.boby3@gmail.com>
2026-05-16 20:43:30 +0300

    made getProfile for both roles

--- routes/userRouter.ts
+++ routes/userRouter.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import { authUser } from "../middleware/authUser.ts";
 import getProfileController from "../Controllers/userControllers/getProfileController.ts";
+import authAdminOrUser from "../middleware/authAdminOrUser.ts";
 const userRouter = express.Router();
-userRouter.use(authUser);
+userRouter.use(authAdminOrUser);
 userRouter.get("/", getProfileController);
 export default userRouter;
