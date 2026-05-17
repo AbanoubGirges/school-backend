02cc7533694d61e5a95da343adc784a2c8b9e9e9
bebofcds <bebo.boby3@gmail.com>
2026-05-17 22:40:59 +0300

    changed to serverless for vercel

--- app.ts
+++ app.ts
@@ -39,7 +39,8 @@ app.use((err: Error|multer.MulterError, req: express.Request, res: express.Respo
 
   next();
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+export default app;
