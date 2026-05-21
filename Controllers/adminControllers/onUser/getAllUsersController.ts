a3d1da016d7e9e89b959818c858c26ea297bf476
bebofcds <bebo.boby3@gmail.com>
2026-05-21 16:20:29 +0300

    added users who did not attend

--- Controllers/adminControllers/onUser/getAllUsersController.ts
+++ Controllers/adminControllers/onUser/getAllUsersController.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import { getUsersDidNotAttend } from "../../../repo/usersDidNotAttendQueries.js";
 import { getAllUsers } from "../../../repo/getAllUsersQueries.js";
-const getAllUsersController = (req: express.Request, res: express.Response) => {
+const getAllUsersController = async (req: express.Request, res: express.Response) => {
     try{
         const isDidNotAttend = req.query.notAttend === "true";
         if(isDidNotAttend){
-            const usersDidNotAttend = getUsersDidNotAttend();
+            const usersDidNotAttend = await getUsersDidNotAttend();
+            if(usersDidNotAttend.length === 0){
+                return res.status(404).json({message: "No users found who did not attend today"});
+            }
             res.json(usersDidNotAttend);
         }else{
-            const users = getAllUsers();
+            const users = await getAllUsers();
             res.json(users);
         }
     }catch(error){
