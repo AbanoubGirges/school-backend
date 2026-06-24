aaed34d148ea026f8ece0cb485312c529e39440f
AbanoubGirges <162204930+AbanoubGirges@users.noreply.github.com>
2026-06-24 16:22:02 +0300

    added docs

--- dist/repo/userDataQueries.js
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../config/prismaConnection.js";
-async function fetchUserData(id) {
-    const user = await prisma.user.findUnique({
-        where: { id },
-    });
-    const { password, ...userData } = user || {};
-    return userData;
-}
-export { fetchUserData };
--- dist/routes/userAttendanceRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import getAttendanceController from "../Controllers/userControllers/getAttendanceController.js";
-import { authUser } from "../middleware/authUser.js";
-const userAttendanceRouter = express.Router();
-userAttendanceRouter.use(authUser);
-userAttendanceRouter.get("/", getAttendanceController);
-export default userAttendanceRouter;
