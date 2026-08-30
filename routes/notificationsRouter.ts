1ebb089614abea1bac2283437165b50b6f6e316f
bebofcds <bebo.boby3@gmail.com>
2026-08-30 17:45:53 +0300

    يا نعيش عيشة فل يا نموت احنا الكل

--- routes/notificationsRouter.ts
+++ routes/notificationsRouter.ts
@@ -2,6 +2,17 @@ import express from "express";
 import { authUser } from "../middleware/authUser.js";
 import {body} from "express-validator";
 import createPushTokenController from "../Controllers/adminControllers/notificationControllers/createPushTokenController.js";
+import { deletePushToken } from "../repo/notificationQueries.js";
 const pushNotificationsRouter=express.Router();
 pushNotificationsRouter.post("/",authUser,[body("expoPushToken").notEmpty().withMessage("Push Notification Required")],createPushTokenController);
+pushNotificationsRouter.delete('/',authUser,async(req:express.Request,res:express.Response)=>{
+    const userId=req.user?.id;
+    try{
+        await deletePushToken(userId);
+        res.status(200).json({message:'token deleted successfully'});
+    }catch(error){
+        console.error(`Failed to delete push token: ${error}`);
+        res.status(500).json({error:'FAILED_TO_DELETE_PUSH_TOKEN'});
+    }
+})
 export default pushNotificationsRouter;
