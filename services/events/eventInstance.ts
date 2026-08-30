4cd9fc6eb05e3cd8ab5379f92b577202f8e9e342
bebofcds <bebo.boby3@gmail.com>
2026-08-30 21:31:41 +0300

    يا نعيش عيشة فل يا نموت احنا الكل

--- services/events/eventInstance.ts
+++ services/events/eventInstance.ts
@@ -1,6 +1,7 @@
 import EventEmitter from 'events';
 import { registrationNotificationService } from '../notifications/registrationNotificationService.js';
+import newAssignmentNotificationService from '../notifications/newAssignmentNotificationService.js';
 const emitter: EventEmitter = new EventEmitter();
 emitter.on('userRegistered', registrationNotificationService);
-emitter.on('newAssignment',)
+emitter.on('newAssignment',newAssignmentNotificationService)
 export default emitter ;
