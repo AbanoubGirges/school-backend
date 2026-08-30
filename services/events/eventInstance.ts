74a0156623d89e6fb8c847a7164502b210808740
bebofcds <bebo.boby3@gmail.com>
2026-08-31 00:59:33 +0300

    added results notification

--- services/events/eventInstance.ts
+++ services/events/eventInstance.ts
@@ -1,8 +1,9 @@
 import EventEmitter from 'events';
 import { registrationNotificationService } from '../notifications/registrationNotificationService.js';
 import newAssignmentNotificationService from '../notifications/newAssignmentNotificationService.js';
+import resultNotificationService from '../notifications/resultNotificationService.js';
 const emitter: EventEmitter = new EventEmitter();
 emitter.on('userRegistered', registrationNotificationService);
 emitter.on('newAssignment',newAssignmentNotificationService);
-emitter.on('newResult',);
+emitter.on('newResult',resultNotificationService);
 export default emitter ;
