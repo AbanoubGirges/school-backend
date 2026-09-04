d842ddc718d5f0a2dcc9b18a2c0c069f2e2da87f
Faaady <47241432+iTzFady@users.noreply.github.com>
2026-09-04 20:35:40 +0300

    fixed create admins

--- Controllers/adminControllers/termControllers/createTermController.ts
+++ Controllers/adminControllers/termControllers/createTermController.ts
@@ -7,29 +7,30 @@ const createTermController = async (
   req: express.Request,
   res: express.Response,
 ) => {
-  try{
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-    return;
-  }
-  const { termName, startDate, endDate} = req.body;
-  
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+    const { termName, startDate, endDate } = req.body;
+
     // Assuming you have a function to create a term in your database
     const user = await fetchUserByUsername(req.user?.userName);
     if (!user) {
       res.status(401).json({ error: "User not found" });
       return;
     }
-    const [_,password]=user;
+    const [_, password] = user;
     const isPasswordValid = await bcrypt.compare(req.body.password, password);
-        if (!isPasswordValid) {
-          res.status(404).json({ error: "INVALID_CREDENTIALS" });
-          return;
-        }
+    if (!isPasswordValid) {
+      res.status(404).json({ error: "INVALID_CREDENTIALS" });
+      return;
+    }
     const term = await newTerm(termName, startDate, endDate);
     res.status(201).json(term);
   } catch (error) {
+    console.error("Error creating term:", error);
     res.status(500).json({ error: "Failed to create term" });
   }
 };
