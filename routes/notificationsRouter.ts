092eef232f11ef6b43811e896d783ad663973aa6
Faaady <47241432+iTzFady@users.noreply.github.com>
2026-09-04 20:42:49 +0300

    added get all notifications

--- routes/notificationsRouter.ts
+++ routes/notificationsRouter.ts
@@ -3,6 +3,7 @@ import { authUser } from "../middleware/authUser.js";
 import { body } from "express-validator";
 import createPushTokenController from "../Controllers/adminControllers/notificationControllers/createPushTokenController.js";
 import { deletePushToken } from "../repo/notificationQueries.js";
+import { getAllNotifications } from "../repo/notificationQueries.js";
 const pushNotificationsRouter = express.Router();
 pushNotificationsRouter.post(
   "/",
@@ -30,6 +31,8 @@ pushNotificationsRouter.get(
   async (req: express.Request, res: express.Response) => {
     const userId = req.user?.id;
     try {
+      const allNotifications = await getAllNotifications(userId);
+      res.status(200).json(allNotifications);
     } catch (error) {
       console.error(`Failed to fetch push token: ${error}`);
       res.status(500).json({ error: "FAILED_TO_FETCH_PUSH_TOKEN" });
